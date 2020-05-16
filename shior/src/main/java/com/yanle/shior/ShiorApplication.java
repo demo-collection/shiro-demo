44d6a9901bb09e2e118e8f1ed64e8fccc7b3e55e
yanlele <331393627@qq.com>
2020-05-16 16:19:04 +0800

    feat: 初始化项目

---
+++ shior/src/main/java/com/yanle/shior/ShiorApplication.java
@@ -0,0 +1,13 @@
+package com.yanle.shior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiorApplication.class, args);
+    }
+
+}
---
+++ shior/src/test/java/com/yanle/shior/ShiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanle.shior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
