ea7b89c588523d10734863ed37ad2f17ce77e547
yanlele <331393627@qq.com>
2020-05-17 22:09:30 +0800

    feat: JWTFilter

--- jwt/src/main/java/com/yanle/shiro/jwt/filter/JWTFilter.java
+++ jwt/src/main/java/com/yanle/shiro/jwt/filter/JWTFilter.java
@@ -10,17 +10,27 @@ import org.springframework.web.client.HttpServerErrorException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
 
 public class JWTFilter extends BasicHttpAuthenticationFilter {
     private Logger logger = LoggerFactory.getLogger(JWTFilter.class);
 
-//    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws UnauthorizedException {
-//        if (isLoginAttempt(request, response)) {
-//            try {
-//
-//            }
-//        }
-//    }
+    /**
+     * 如果带有 token，则对 token 进行检查，否则直接通过
+     */
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws UnauthorizedException {
+        if (isLoginAttempt(request, response)) {
+            try {
+                executeLogin(request, response);
+                return true;
+            } catch (Exception e) {
+                // token 错误
+                responseError(response, e.getMessage());
+            }
+        }
+    }
 
     /**
      * 执行登陆操作
@@ -36,5 +46,18 @@ public class JWTFilter extends BasicHttpAuthenticationFilter {
 
         // 如果没有抛出错误， 说明登录成功
         return true;
+    }
+
+    /**
+     * 将非法请求跳转到 /unauthorized/**
+     */
+    private void responseError(ServletResponse response, String message) {
+        try {
+            HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+            message = URLEncoder.encode(message, "UTF-8");
+            httpServletResponse.sendRedirect("/unauthorized" + message);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
     }
 }
