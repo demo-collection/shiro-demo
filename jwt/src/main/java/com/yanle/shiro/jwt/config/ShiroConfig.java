435ebc904f4a9569605ed1116146a0d70f0726b7
yanlele <331393627@qq.com>
2020-05-17 12:58:55 +0800

    feat: shiro 方法和配置

---
+++ jwt/src/main/java/com/yanle/shiro/jwt/config/ShiroConfig.java
@@ -0,0 +1,14 @@
+package com.yanle.shiro.jwt.config;
+
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShiroConfig {
+
+//    @Bean
+//    public ShiroFilterFactoryBean factoryBean() {
+//
+//    }
+}
